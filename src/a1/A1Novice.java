317c12116dab41620d26409006d173e6c11ac0a8
lintanying <lintanying@lintanyingdeair.wireless-1x.unc.edu>
2019-08-31 15:51:23 -0400

    try and try

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -2,7 +2,8 @@ package a1;
 
 import java.util.Scanner;
 
-public class A1Novice {
+public class A1Novice
+{
 
 	public static void main(String[] args) {
 		
@@ -10,19 +11,20 @@ public class A1Novice {
 
 		// Your code follows here.
 		int numCustomers = scan.nextInt();
-		double total = 0.0;
 		
 		for(int i = 0; i<numCustomers; i++)
 		{
-			
+			double total = 0.0;
 			String name = scan.next().substring(0, 1) + "." + " " + scan.next();
-			
-			for(int j = 0; j < scan.nextInt(); j++)
+			int times = scan.nextInt();
+			for(int j = 0; j < times; j++)
 			{
-				total = total + scan.nextInt() * scan.nextDouble();
+				int number = scan.nextInt();
+				scan.next();
+				total = total + number * scan.nextDouble();
 			}
-			//System.out.println(name + ":" + " " + total);
-			System.out.print(total);
+			System.out.println(name + ":" + " " + total);
+			//System.out.print(total);
 			
 		}
 		
