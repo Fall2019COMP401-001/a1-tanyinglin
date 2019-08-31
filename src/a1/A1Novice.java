0b249700519526d78e0c0b9ee859de01243774c6
lintanying <lintanying@lintanyingdeair.wireless-1x.unc.edu>
2019-08-31 10:54:29 -0400

    try

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,22 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int numCustomers = scan.nextInt();
+		double total = 0.0;
+		
+		for(int i = 0; i<numCustomers; i++)
+		{
+			
+			String name = scan.next().substring(0, 1) + "." + " " + scan.next();
+			
+			for(int j = 0; j < scan.nextInt(); j++)
+			{
+				total = total + scan.nextInt() * scan.nextDouble();
+			}
+			//System.out.println(name + ":" + " " + total);
+			System.out.print(total);
+			
+		}
 		
 	}
 }
