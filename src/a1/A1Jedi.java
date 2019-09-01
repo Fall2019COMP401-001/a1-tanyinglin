387d5de26e2825d91b2959ebbfc7cd174eb3eef6
lintanying <lintanying@152.23.59.101>
2019-09-01 15:00:25 -0400

    finally

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -2,13 +2,60 @@ package a1;
 
 import java.util.Scanner;
 
-public class A1Jedi {
+public class A1Jedi 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
 		
+		int totalAmount = scan.nextInt();
+		double[] prices = new double[totalAmount];
+		String[] brands = new String[totalAmount];
+		int[] timesGoods = new int [totalAmount];
+		
+		for(int i = 0; i < totalAmount; i++)
+		{
+			brands[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+	    
+		int totalCustomers = scan.nextInt();
+		int[] timesCustomers = new int [totalAmount];
+		int[] timesItems = new int [totalAmount];
+		for(int i = 0; i<totalCustomers; i++)
+		{
+			scan.next();
+			scan.next();
+			int howManyItems = scan.nextInt();
+			for(int j = 0; j<howManyItems; j++)
+			{
+				int howManyThis = scan.nextInt();
+				String brandName = scan.next();
+				for(int k = 0; k <totalAmount; k++)
+				{
+					
+					if(brandName.equals(brands[k]))
+					{
+						//index = k;
+						//timesCustomers[k] = timesCustomers[k] + howManyThis;
+						timesCustomers[k]++;
+						timesItems[k]= timesItems[k] + howManyThis;
+					}
+				}
+				
+			}
+		}
+		for(int i = 0; i < totalAmount; i++)
+		{
+			if(timesItems[i]==0)
+				System.out.println("NO Customers bought " + brands[i]);
+			else
+				System.out.println(timesCustomers[i]+" "+"Customers bought "+timesItems[i]+" "+brands[i]);
+		}
 	}
+
 }
